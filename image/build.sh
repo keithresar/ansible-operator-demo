c7ed984c770cc2e42b9e7579170679fedcdece94
root <root@master.ocp11.lab.422long.com>
2018-11-01 14:02:57 -0500

    -

---
+++ image/build.sh
@@ -0,0 +1,20 @@
+
+version=${1}
+
+docker build -t "docker.io/hk1232/operator-hello-world:latest" .
+docker tag docker.io/hk1232/operator-hello-world:latest docker.io/hk1232/operator-hello-world:0.${version}
+docker push docker.io/hk1232/operator-hello-world:0.${version}
+
+echo "-------------------------------------------"
+echo ""
+echo "Now to publish the image in dockerhub do:"
+echo ""
+echo " docker login docker.io # With your docker credentials"
+echo ""
+echo " docker tag openshiftroadshow/parksmap:latest openshiftroadshow/parksmap:<VERSION>"
+echo ""
+echo " docker push openshiftroadshow/parksmap:<VERSION>"
+echo ""
+echo " NOTE: Replace <VERSION> with the actual version of the image you're building"
+echo ""
+echo "-------------------------------------------"
---
+++ image/hello_world.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello world executing"
+
+env
+
+while true; do
+	sleep 2
+done
+
+exit 0
+
