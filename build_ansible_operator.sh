b2a763d466fc1a65709f5a3cbc25f6c8d887d083
root <root@master.ocp11.lab.422long.com>
2018-11-05 16:30:14 -0600

    s/hello-world/simpledb

--- build_ansible_operator.sh
+++ build_ansible_operator.sh
@@ -1,20 +1,8 @@
 
-version=${1}
+#version=${1}
+version=1
 
 docker build -t "docker.io/hk1232/operator-hello-world-runner:latest" .
-docker tag docker.io/hk1232/operator-hello-world-runner:latest docker.io/hk1232/operator-hello-world-runner:0.${version}
-docker push docker.io/hk1232/operator-hello-world-runner:0.${version}
+docker tag docker.io/hk1232/operator-simpledb-runner:latest docker.io/hk1232/operator-simpledb-runner:0.${version}
+docker push docker.io/hk1232/operator-simpledb-runner:0.${version}
 
-echo "-------------------------------------------"
-echo ""
-echo "Now to publish the image in dockerhub do:"
-echo ""
-echo " docker login docker.io # With your docker credentials"
-echo ""
-echo " docker tag openshiftroadshow/parksmap:latest openshiftroadshow/parksmap:<VERSION>"
-echo ""
-echo " docker push openshiftroadshow/parksmap:<VERSION>"
-echo ""
-echo " NOTE: Replace <VERSION> with the actual version of the image you're building"
-echo ""
-echo "-------------------------------------------"
