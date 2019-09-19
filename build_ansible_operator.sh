473ecacd735a71d54b9508cc921167adac3ad028
Keith Resar <keithresar@Keiths-Mac-mini.local>
2019-09-19 10:42:14 -0500

    -

--- build_ansible_operator.sh
+++ build_ansible_operator.sh
@@ -1,8 +1,12 @@
 
 #version=${1}
-version=1
+version=2
 
-docker build -t "docker.io/hk1232/operator-simpledb-runner:latest" .
-docker tag docker.io/hk1232/operator-simpledb-runner:latest docker.io/hk1232/operator-simpledb-runner:0.${version}
-docker push docker.io/hk1232/operator-simpledb-runner:0.${version}
+#docker build -t "docker.io/hk1232/operator-simpledb-runner:latest" .
+#docker tag docker.io/hk1232/operator-simpledb-runner:latest docker.io/hk1232/operator-simpledb-runner:0.${version}
+#docker push docker.io/hk1232/operator-simpledb-runner:0.${version}
+
+docker build -t "quay.io/keithresar/operator-simpledb-runner:latest" .
+docker tag quay.io/keithresar/operator-simpledb-runner:latest quay.io/keithresar/operator-simpledb-runner:0.${version}
+docker push quay.io/keithresar/operator-simpledb-runner:0.${version}
 
