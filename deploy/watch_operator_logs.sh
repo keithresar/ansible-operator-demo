ef4a41c36eb121bad87fb0f8888fd2aa99d34824
Keith Resar <keith.resar@422long.com>
2019-09-25 16:05:30 -0400

    -

--- deploy/watch_operator_logs.sh
+++ deploy/watch_operator_logs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-oc logs -f `oc get pods | grep simpledb-operator | awk '{ print $1 }'` | grep "playbook task"
+oc logs -f `oc get pods | grep simpledb-operator | awk '{ print $1 }'` | \
+    grep "playbook task" 
 
